ad58de104c07d11e86fdb768dea637ff057ab46b
my-portfolio <eliasephrata@gmail.com>
2025-05-27 09:11:40 +0300

    first commit

---
+++ src/components/todolist.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './todolist.css';
+
+function Todolist() {
+  return (
+    <div className='todo-container'>
+        <h1>TO-DO List</h1>
+        <div className="input-section">
+            <input type="text" className="input-field" />
+            <>
+            <button className="update-btn"></button>
+            <button className="cancel-btn"></button>
+
+            </>
+        </div>
+    </div>
+   
+
+  )
+}
+
+export default Todolist
